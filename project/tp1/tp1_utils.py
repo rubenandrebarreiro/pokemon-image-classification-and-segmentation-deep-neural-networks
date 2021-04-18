242d319fc8df9bad9f9735d267ad36b6e3fd3793
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-18 18:15:51 +0100

    Some minor changes and rearrangements on the Module with Auxiliary Functions given by the Instructors added

--- project/tp1/tp1_utils.py
+++ project/tp1/tp1_utils.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Aprendizagem Profunda, TP1
 
-Auxiliary functions
+Deep Learning - Assignment #1:
+- Pokemon Image Classification and Segmentation with Deep Neural Networks
+
+Integrated Master of Computer Science and Engineering
+
+NOVA School of Science and Technology,
+NOVA University of Lisbon - 2020/2021
+
+Authors:
+- Rodrigo Jorge Ribeiro (rj.ribeiro@campus.fct.unl.pt) - Student no. 49067
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt) - Student no. 42648
+
+Instructor(s):
+- Ludwig Krippahl (ludi@fct.unl.pt)
+- Claudia Soares (claudia.soares@fct.unl.pt)
+
+Module provided by the Instructor(s) with Auxiliary Functions for the Project
 
 """
 
@@ -13,7 +28,7 @@ import pandas as pd
 from tensorflow.keras.utils import to_categorical
 
 
-def images_to_pic(f_name,images,width = 20):
+def images_to_pic(f_name, images, width=20):
     """
     Saves a single image file with the images provided in the images array.
     Example:
@@ -21,22 +36,28 @@ def images_to_pic(f_name,images,width = 20):
         images_to_pic('test_set.png',data['test_X'])
     """
     rows = - (len(images)//-width)
+
     image_hei = images[0].shape[0]
     image_wid = images[0].shape[1]
-    picture = np.zeros( (rows*image_hei,width*image_wid,3))
-    row  = col = 0
+
+    picture = np.zeros((rows*image_hei, width*image_wid, 3))
+    row = col = 0
+
     for img in images:
         if len(img.shape) == 2:
-            img = img[:,:,np.newaxis]
-        picture[row*image_hei:(row+1)*image_hei,col*image_wid:(col+1)*image_wid] = img
-        col+=1
-        if col>=width:
+            img = img[:, :, np.newaxis]
+
+        picture[row*image_hei:(row+1)*image_hei, col*image_wid:(col+1)*image_wid] = img
+        col += 1
+
+        if col >= width:
             col = 0
             row += 1
             
-    imsave(f_name,picture)
-    
-def compare_masks(f_name,masks1,masks2,width = 20):
+    imsave(f_name, picture)
+
+
+def compare_masks(f_name, masks1, masks2, width=20):
     """
     Creates a single image file comparing the two sets of masks
     Red pixels are pixels in mask1 but not in mask2
@@ -47,15 +68,21 @@ def compare_masks(f_name,masks1,masks2,width = 20):
         compare_masks('test_compare.png',data['test_masks'],predicts)
     """
     imgs = []
-    for m1,m2 in zip(masks1,masks2):
-        img = np.zeros((m1.shape[0],m1.shape[1],3))        
-        img[m1[:,:,0]>0.5,0] = 1
-        img[m2[:,:,0]>0.5,1] = 1
-        img[np.logical_and(m1[:,:,0]>0.5,m2[:,:,0]>0.5),2] = 1
+
+    for m1, m2 in zip(masks1, masks2):
+
+        img = np.zeros((m1.shape[0], m1.shape[1], 3))
+
+        img[m1[:, :, 0] > 0.5, 0] = 1
+        img[m2[:, :, 0] > 0.5, 1] = 1
+
+        img[np.logical_and(m1[:, :, 0] > 0.5, m2[:, :, 0] > 0.5), 2] = 1
         imgs.append(img)
-    images_to_pic(f_name,imgs,width)
-    
-def overlay_masks(f_name,images,masks,width = 20):
+
+    images_to_pic(f_name, imgs, width)
+
+
+def overlay_masks(f_name, images, masks, width=20):
     """
     Creates a single image file overlaying the masks and the images
     Region outside the masks are turned red and darker
@@ -64,13 +91,17 @@ def overlay_masks(f_name,images,masks,width = 20):
         overlay_masks('test_overlay.png',data['test_X'],predicts)
     """
     imgs = []
-    for im,m in zip(images,masks):
+    for im, m in zip(images, masks):
         img = np.copy(im)
-        mask = m[:,:,0]<0.5
-        img[mask,1:] = img[mask,1:]/10
-        img[mask,0] = (1+img[mask,0])/2
+
+        mask = m[:, :, 0] < 0.5
+
+        img[mask, 1:] = img[mask, 1:]/10
+        img[mask, 0] = (1+img[mask, 0])/2
+
         imgs.append(img)
-    images_to_pic(f_name,imgs,width)
+
+    images_to_pic(f_name, imgs, width)
 
     
 def load_data():
@@ -93,20 +124,24 @@ def load_data():
     mask = ~table['secondary_type_index'].isnull()
     rows = np.arange(len(mask))[mask]
     secondary_ixs = table['secondary_type_index'][mask].values.astype(int)    
-    labels[tuple((rows,secondary_ixs))] = 1
+    labels[tuple((rows, secondary_ixs))] = 1
     
-    return     {'train_X':data['images'][:4000],
-                'test_X':data['images'][4000:],
-                'train_masks':data['masks'][:4000],
-                'test_masks':data['masks'][4000:],
-                'train_classes':classes[:4000],
-                'train_labels':labels[:4000],
-                'test_classes':classes[4000:],
-                'test_labels':labels[4000:],}
-    
+    return {'train_X': data['images'][:4000],
+            'test_X': data['images'][4000:],
+            'train_masks': data['masks'][:4000],
+            'test_masks': data['masks'][4000:],
+            'train_classes': classes[:4000],
+            'train_labels': labels[:4000],
+            'test_classes': classes[4000:],
+            'test_labels': labels[4000:], }
+
+
 if __name__ == '__main__':
+
     ds = load_data()
+
     for k in ds.keys():
-        print(k,ds[k].shape)
-    images_to_pic('tp1_sample.png',ds['train_X'][:100])
-    images_to_pic('tp1_sample_masks.png',ds['train_masks'][:100])
+        print(k, ds[k].shape)
+
+    images_to_pic('tp1_sample.png', ds['train_X'][:100])
+    images_to_pic('tp1_sample_masks.png', ds['train_masks'][:100])
