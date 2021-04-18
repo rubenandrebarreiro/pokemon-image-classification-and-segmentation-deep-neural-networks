0c2162882a59b7157fb74060580b1a48330848d9
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-18 21:49:31 +0100

    More developments added on the Convolutional Neural Network (C.N.N.) Model, with Keras Sequential API

--- project/tp1/tp1.py
+++ project/tp1/tp1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 
 Deep Learning - Assignment #1:
@@ -37,8 +39,26 @@ from tensorflow import keras
 # Import the Sequential from the TensorFlow.Keras.Models Python's Module
 from tensorflow.keras.models import Sequential
 
-# Import the Conv2D from the TensorFlow.Keras.Layers Module
+# Import the Convolution 2D Layer from the TensorFlow.Keras.Layers Module
 from tensorflow.keras.layers import Conv2D
+
+# Import the Activation Layer from the TensorFlow.Keras.Layers Module
+from tensorflow.keras.layers import Activation
+
+# Import the Batch Normalization Layer from the TensorFlow.Keras.Layers Module
+from tensorflow.keras.layers import BatchNormalization
+
+# Import the Max Pooling 2D Layer from the TensorFlow.Keras.Layers Module
+from tensorflow.keras.layers import MaxPooling2D
+
+# Import the Dropout Layer from the TensorFlow.Keras.Layers Module
+from tensorflow.keras.layers import Dropout
+
+# Import the Flatten Layer from the TensorFlow.Keras.Layers Module
+from tensorflow.keras.layers import Flatten
+
+# Import the Dense Layer from the TensorFlow.Keras.Layers Module
+from tensorflow.keras.layers import Dense
 
 # Import the Auxiliary Function to Load the Data for the Project, from the TP1_Utils' Module
 from tp1_utils import load_data
@@ -72,7 +92,7 @@ NUM_CHANNELS_GRAY_SCALE = 1
 
 # The Number of Filters for the Model of
 # the Convolution Neural Network (C.N.N.)
-NUM_FILTERS = [32, 64]
+NUM_FILTERS = [32, 32, 64, 64]
 
 # The Height of the Kernel of the Filters used for the Model of
 # the Convolution Neural Network (C.N.N.)
@@ -82,9 +102,27 @@ KERNEL_HEIGHT = 3
 # the Convolution Neural Network (C.N.N.)
 KERNEL_WIDTH = 3
 
+# The Height of the Pooling Matrix used for the Model of
+# the Convolution Neural Network (C.N.N.)
+POOLING_HEIGHT = 2
+
+# The Width of the Pooling Matrix used for the Model of
+# the Convolution Neural Network (C.N.N.)
+POOLING_WIDTH = 2
+
+# The Height of the Stride used on
+# the Pooling Matrices used for the Model of
+# the Convolution Neural Network (C.N.N.)
+STRIDE_HEIGHT = 2
+
+# The Width of the Stride used on
+# the Pooling Matrices used for the Model of
+# the Convolution Neural Network (C.N.N.)
+STRIDE_WIDTH = 2
+
 # The Optimisers available to use for the the Model of
 # the Convolution Neural Network (C.N.N.)
-AVAILABLE_OPTIMISERS = ["SGD", "RMSPROP", "ADAM", "ADAGRAD", "ADADELTA"]
+AVAILABLE_OPTIMISERS_LIST = ["SGD", "RMSPROP", "ADAM", "ADAGRAD", "ADADELTA"]
 
 # The Learning Rate for the Optimizer used for
 # the Model of the Convolution Neural Network (C.N.N.)
@@ -150,25 +188,203 @@ def retrieve_datasets_from_pokemon_data():
         xs_features_testing_set, xs_masks_testing_set, ys_classes_testing_set, ys_labels_testing_set
 
 
-# Create a Model for a Convolution Neural Network (C.N.N.)
+# Create a Model for a feed-forward Convolution Neural Network (C.N.N.)
 def create_cnn_model_in_keras_sequential_api_for_image_classification():
 
-    # Create the Tensorflow Keras' Sequential API,
-    # for a feed-forward Convolution Neural Network (C.N.N.),
-    # which is most appropriate for this type of problem
-    # (i.e., Image Classification)
+    # Create a Model for a feed-forward Convolution Neural Network (C.N.N.),
+    # which is most appropriate for this type of problem (i.e., Image Classification),
+    # using the Tensorflow Keras' Sequential API
     convolution_neural_network_tensorflow_keras_sequential_model = Sequential()
 
     # 1st CONV => RELU => CONV => RELU => POOL layer set
 
-    # Add a first Convolution 2D Matrix, for the Input Data (Images) of
-    # the Pokemons' Dataset given to the feed-forward
+    # Add a first Convolution 2D Layer, for the Input features of the Data/Images of
+    # the Pokemons' Dataset given to the Model of the feed-forward
     # Convolution Neural Network (C.N.N.), using 32 Filters of a Kernel 3x3,
-    # Same Padding and an Input Shape having a Batch Size of 28, with 28 Steps, as also,
-    # 1 Input Dimension (for one Color Channel - Grayscale Color)
+    # Same Padding and an Input Shape of (64 x 64 pixels), as also,
+    # 3 Input Dimensions (for each Color Channel - RGB Color)
     convolution_neural_network_tensorflow_keras_sequential_model\
         .add(Conv2D(NUM_FILTERS[0], (KERNEL_HEIGHT, KERNEL_WIDTH), padding="same",
              input_shape=(IMAGES_HEIGHT, IMAGES_WIDTH, NUM_CHANNELS_RGB)))
+
+    # Add a Rectified Linear Unit (ReLU) as Activation Function Layer,
+    # for the features of the Data/Images of the Pokemons resulted from
+    # the previous Layer of the Model of the feed-forward
+    # Convolution Neural Network (C.N.N.)
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Activation("relu"))
+
+    # Add a Batch Normalization Layer, to normalize the features of
+    # the Data/Images of the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # by re-centering and re-scaling that features and making the Model of
+    # the feed-forward Convolution Neural Network (C.N.N.) to be faster and more stable
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(BatchNormalization(axis=-1))
+
+    # Add a second Convolution 2D Layer, for the previous features of the Data/Images of
+    # the Pokemons' Dataset given to the Model of the feed-forward
+    # Convolution Neural Network (C.N.N.), resulted from the previous layer,
+    # using 32 Filters of a Kernel 3x3 and Same Padding
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Conv2D(NUM_FILTERS[1], (KERNEL_HEIGHT, KERNEL_WIDTH), padding="same"))
+
+    # Add a Rectified Linear Unit (ReLU) as Activation Function Layer,
+    # for the features of the Data/Images of the Pokemons resulted from
+    # the previous Layer of the Model of the feed-forward
+    # Convolution Neural Network (C.N.N.)
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Activation("relu"))
+
+    # Add a Batch Normalization Layer, to normalize the features of
+    # the Data/Images of the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # by re-centering and re-scaling that features and making the Model of
+    # the feed-forward Convolution Neural Network (C.N.N.) to be faster and more stable
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(BatchNormalization(axis=-1))
+
+    # Add a Maximum Pooling 2D Sample-Based Discretization Process Layer,
+    # for the features of the Data/Images of the Pokemons resulted from
+    # the previous layer of the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # with a 2x2 Pooling Size and 2x2 Stride Size
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(MaxPooling2D(pool_size=(POOLING_HEIGHT, POOLING_WIDTH),
+                          strides=(STRIDE_HEIGHT, STRIDE_WIDTH)))
+
+    # Add a Dropout Layer, for Regularization of the features of
+    # the Data/Images of the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # using as hyper-parameter, the Dropout Rate of 25%
+    # NOTE:
+    # - Dropout Layer in Convolution Neural Networks is generally, not very useful;
+    # - Comment/Uncomment, if you want to try it or not;
+    convolution_neural_network_tensorflow_keras_sequential_model.add(Dropout(0.25))
+
+    # 2nd CONV => RELU => CONV => RELU => POOL layer set
+
+    # Add a third Convolution 2D Layer, for the previous features of the Data/Images of
+    # the Pokemons' Dataset given to the Model of the feed-forward
+    # Convolution Neural Network (C.N.N.), resulted from the previous layer,
+    # using 64 Filters of a Kernel 3x3 and Same Padding
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Conv2D(NUM_FILTERS[2], (KERNEL_HEIGHT, KERNEL_WIDTH), padding="same"))
+
+    # Add a Rectified Linear Unit (ReLU) as Activation Function Layer,
+    # for the features of the Data/Images of the Pokemons resulted from
+    # the previous layer of the Model of the feed-forward Convolution Neural Network (C.N.N.)
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Activation("relu"))
+
+    # Add a Batch Normalization Layer, to normalize the features of
+    # the Data/Images of the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # by re-centering and re-scaling that features and making the Model of
+    # the feed-forward Convolution Neural Network (C.N.N.) to be faster and more stable
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(BatchNormalization(axis=-1))
+
+    # Add a fourth Convolution 2D Layer, for the previous features of the Data/Images of
+    # the Pokemons' Dataset given to the Model of the feed-forward
+    # Convolution Neural Network (C.N.N.), resulted from the previous layer,
+    # using 64 Filters of a Kernel 3x3 and Same Padding
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Conv2D(NUM_FILTERS[3], (KERNEL_HEIGHT, KERNEL_WIDTH), padding="same"))
+
+    # Add a Rectified Linear Unit (ReLU) as Activation Function Layer,
+    # for the features of the Data/Images of the Pokemons resulted from
+    # the previous Layer of the Model of the feed-forward
+    # Convolution Neural Network (C.N.N.)
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Activation("relu"))
+
+    # Add a Batch Normalization Layer, to normalize the features of
+    # the Data/Images of the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # by re-centering and re-scaling that features and making the Model of
+    # the feed-forward Convolution Neural Network (C.N.N.) to be faster and more stable
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(BatchNormalization(axis=-1))
+
+    # Add a Maximum Pooling 2D Sample-Based Discretization Process Layer,
+    # for the features of the Data/Images of the Pokemons resulted from
+    # the previous layer of the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # with a 2x2 Pooling Size and 2x2 Stride Size
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(MaxPooling2D(pool_size=(POOLING_HEIGHT, POOLING_WIDTH),
+                          strides=(STRIDE_HEIGHT, STRIDE_WIDTH)))
+
+    # Add a Dropout Layer, for Regularization of the features of
+    # the Data/Images of the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # using as hyper-parameter, the Dropout Rate of 25%
+    # NOTE:
+    # - Dropout Layer in Convolution Neural Networks is generally, not very useful;
+    # - Comment/Uncomment, if you want to try it or not;
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Dropout(0.25))
+
+    # 1st (and only) set of FC => RELU layers
+
+    # Add a Flatten Layer to the features of the Data/Images of
+    # the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.)
+    # NOTE:
+    # - This is needed to flatten the input into a single dimension for the features,
+    #   which is what the next Dense Layer needs;
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Flatten())
+
+    # Add a Dense Layer to the features of the Data/Images of
+    # the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # for a total of 512 Units (Weights and Biases)
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Dense(512))
+
+    # Add a Rectified Linear Unit (ReLU) as Activation Function Layer,
+    # for the features of the Data/Images of the Pokemons resulted from
+    # the previous Layer of the Model of the feed-forward
+    # Convolution Neural Network (C.N.N.)
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Activation("relu"))
+
+    # Add a Batch Normalization Layer, to normalize the features of
+    # the Data/Images of the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # by re-centering and re-scaling that features and making the Model of
+    # the feed-forward Convolution Neural Network (C.N.N.) to be faster and more stable
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(BatchNormalization())
+
+    # Add a Dropout Layer, for Regularization of the features of
+    # the Data/Images of the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # using as hyper-parameter, the Dropout Rate of 50%
+    # NOTE:
+    # - Dropout Layer in Convolution Neural Networks is generally, not very useful;
+    # - Comment/Uncomment, if you want to try it or not;
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Dropout(0.5))
+
+    # SoftMax Classifier
+
+    # Add a Dense Layer to the features of the Data/Images of
+    # the Pokemons resulted from the previous Layer of
+    # the Model of the feed-forward Convolution Neural Network (C.N.N.),
+    # for a total of 10 Units (Weights and Biases)
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Dense(10))
+
+    # Add a Softmax as Activation Function Layer,
+    # for the features of the Data/Images of the Pokemons resulted from
+    # the previous Layer of the Model of the feed-forward
+    # Convolution Neural Network (C.N.N.)
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Activation("softmax"))
+
+    # Return the Model of the feed-forward Convolution Neural Network (C.N.N.)
+    return convolution_neural_network_tensorflow_keras_sequential_model
 
 
 # Retrieve all the Datasets related to the Pokemons' Data:
