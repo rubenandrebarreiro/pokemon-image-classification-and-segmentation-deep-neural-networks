7f20ee3c193125e136e4bf38d8ca097ff76ce4b3
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-18 19:44:23 +0100

    First developments added on a Convolutional Neural Network (C.N.N.) Model, with Keras Sequential API

--- project/tp1/tp1.py
+++ project/tp1/tp1.py
@@ -31,25 +31,77 @@ import os as operative_system
 # Disable all the Debugging Logs from TensorFlow Library
 operative_system.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+# Import Keras from the TensorFlow Python's Library
+from tensorflow import keras
+
+# Import the Sequential from the TensorFlow.Keras.Models Python's Module
+from tensorflow.keras.models import Sequential
+
+# Import the Conv2D from the TensorFlow.Keras.Layers Module
+from tensorflow.keras.layers import Conv2D
+
 # Import the Auxiliary Function to Load the Data for the Project, from the TP1_Utils' Module
 from tp1_utils import load_data
 
 
 # Constants
 
-# The Width of the Images/Pictures
+# The Number of Examples (Images/Masks) for the final Training Set
+NUM_EXAMPLES_FINAL_TRAINING_SET = 3500
+
+# The Number of Examples (Images/Masks) for the final Validation Set
+NUM_EXAMPLES_FINAL_VALIDATION_SET = 500
+
+# The Number of Examples (Images/Masks) for the final Testing Set
+NUM_EXAMPLES_FINAL_TESTING_SET = 500
+
+# The Height of the Images/Pictures (64 pixels)
+IMAGES_HEIGHT = 64
+
+# The Width of the Images/Pictures (64 pixels)
 IMAGES_WIDTH = 64
 
-# The Height of the Images/Pictures
-IMAGES_HEIGHT = 64
+# The Size of the Images/Pictures (64 x 64 pixels)
+IMAGE_SIZE = (IMAGES_WIDTH * IMAGES_HEIGHT)
 
 # The Number of Channels for RGB Colors
 NUM_CHANNELS_RGB = 3
 
-# The Number of Channels for Black and White Colors
-NUM_CHANNELS_BW = 1
+# The Number of Channels for Grayscale Colors
+NUM_CHANNELS_GRAY_SCALE = 1
+
+# The Number of Filters for the Model of
+# the Convolution Neural Network (C.N.N.)
+NUM_FILTERS = [32, 64]
+
+# The Height of the Kernel of the Filters used for the Model of
+# the Convolution Neural Network (C.N.N.)
+KERNEL_HEIGHT = 3
+
+# The Width of the Kernel of the Filters used for the Model of
+# the Convolution Neural Network (C.N.N.)
+KERNEL_WIDTH = 3
+
+# The Optimisers available to use for the the Model of
+# the Convolution Neural Network (C.N.N.)
+AVAILABLE_OPTIMISERS = ["SGD", "RMSPROP", "ADAM", "ADAGRAD", "ADADELTA"]
+
+# The Learning Rate for the Optimizer used for
+# the Model of the Convolution Neural Network (C.N.N.)
+INITIAL_LEARNING_RATE = 0.05
+
+# The Number of Epochs for the Optimiser for
+# the Model of the Convolution Neural Network (C.N.N.)
+NUM_EPOCHS = 1000
+
+# The Size of the Batch for the Model of
+# the Convolution Neural Network (C.N.N.)
+BATCH_SIZE = 32
 
 
+# Retrieve the Datasets from the Pokemons' Data,
+# in order to be used to build the model for the Model of
+# the Convolution Neural Network (C.N.N.)
 def retrieve_datasets_from_pokemon_data():
 
     # Load the Data needed for the Project, using the given Auxiliary Function for it,
@@ -98,6 +150,27 @@ def retrieve_datasets_from_pokemon_data():
         xs_features_testing_set, xs_masks_testing_set, ys_classes_testing_set, ys_labels_testing_set
 
 
+# Create a Model for a Convolution Neural Network (C.N.N.)
+def create_cnn_model_in_keras_sequential_api_for_image_classification():
+
+    # Create the Tensorflow Keras' Sequential API,
+    # for a feed-forward Convolution Neural Network (C.N.N.),
+    # which is most appropriate for this type of problem
+    # (i.e., Image Classification)
+    convolution_neural_network_tensorflow_keras_sequential_model = Sequential()
+
+    # 1st CONV => RELU => CONV => RELU => POOL layer set
+
+    # Add a first Convolution 2D Matrix, for the Input Data (Images) of
+    # the Pokemons' Dataset given to the feed-forward
+    # Convolution Neural Network (C.N.N.), using 32 Filters of a Kernel 3x3,
+    # Same Padding and an Input Shape having a Batch Size of 28, with 28 Steps, as also,
+    # 1 Input Dimension (for one Color Channel - Grayscale Color)
+    convolution_neural_network_tensorflow_keras_sequential_model\
+        .add(Conv2D(NUM_FILTERS[0], (KERNEL_HEIGHT, KERNEL_WIDTH), padding="same",
+             input_shape=(IMAGES_HEIGHT, IMAGES_WIDTH, NUM_CHANNELS_RGB)))
+
+
 # Retrieve all the Datasets related to the Pokemons' Data:
 # i) 4000 examples for the initial Training Set, split in:
 #    - 3500 examples for the final Training Set;
@@ -110,5 +183,3 @@ xs_features_training_set_pokemon, xs_masks_training_set_pokemon,\
     xs_features_testing_set_pokemon, xs_masks_testing_set_pokemon,\
     ys_classes_testing_set_pokemon, ys_labels_testing_set_pokemon = \
     retrieve_datasets_from_pokemon_data()
-
-
