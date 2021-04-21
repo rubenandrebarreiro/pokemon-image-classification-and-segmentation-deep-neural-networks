32f8db26b402f419634b10feca14c11a6ab1b9dd
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-21 09:26:46 +0100

    Some Scripts for TensorFlow GPU Tests added

---
+++ others/tensorflow-gpu-test-1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Deep Learning - Assignment #1:
+- Pokemon Image Classification and Segmentation with Deep Neural Networks
+
+Integrated Master of Computer Science and Engineering
+
+NOVA School of Science and Technology,
+NOVA University of Lisbon - 2020/2021
+
+Authors:
+- Rodrigo Jorge Ribeiro (rj.ribeiro@campus.fct.unl.pt) - Student no. 49067
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt) - Student no. 42648
+
+Instructor(s):
+- Ludwig Krippahl (ludi@fct.unl.pt)
+- Claudia Soares (claudia.soares@fct.unl.pt)
+
+Simple Test for Tensorflow GPUs #1
+
+"""
+
+# Import the TensorFlow Python's Library as TensorFlow alias
+import tensorflow as tensorflow
+
+# Print just a Debug message for the TensorFlow-GPU
+print("Number of GPUs Available: ", len(tensorflow.config.list_physical_devices('GPU')))
